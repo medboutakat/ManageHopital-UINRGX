53fb7e815411dc949762557f323a02c066396d24
AbdelwahedK <57575879+AbdelwahedK@users.noreply.github.com>
2020-04-11 01:25:21 +0100

    some changes in invoice

--- src/app/invoices/invoice/invoice.component.ts
+++ src/app/invoices/invoice/invoice.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { formatDate } from '@angular/common';
+import  *  as  data  from 'src/app/data.json';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice',
@@ -7,12 +9,60 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent implements OnInit {
+//detail code
+  products: any = (data as any).default;
+  total=0;
+  show:boolean = false;
+  productForm = new FormGroup({
+    id:new FormControl(0),
+    name:new FormControl(''),
+    description:new FormControl(''),
+    tax:new FormControl(''),
+    price:new FormControl(''),
+    quantity:new FormControl(''),
+    total:new FormControl(this.total)
+  })
+
+  showAdd(){
+    this.show = !this.show;
+  }
+
+  addDetail(){
+    var a = 0
+    this.products.forEach(item =>{
+      item.id > a ? item.id = a : null
+    });
+    console.log('a = ',a)
+    this.productForm.setControl('id',new FormControl(a+1))
+    this.products.push(this.productForm.value)
+    console.log('product form : ',this.productForm.value)
+    console.log(this.products)
+    this.showAdd();
+    localStorage.setItem('data', JSON.stringify(this.products));
+    this.productForm.reset();
+  }
+ //end detail code
+
+ invoiceForm = new FormGroup({
+  id:new FormControl('0'),
+  code: new FormControl(''),
+  date: new FormControl(formatDate(new Date(), 'dd/MM/yyyy', 'en')),
+  totalAmont: new FormControl(0),
+  expedition: new FormControl(0),
+  livraison: new FormControl(0),
+  remise: new FormControl(),
+  invoiceDetails: new FormControl([])
+ })
 
   constructor() { }
 
-  myDate
+  
   ngOnInit(): void {
-    this.myDate = formatDate(new Date(), 'dd/MM/yyyy', 'en');
+    // this.myDate 
+  }
+
+  addInvoice(){
+    console.log('invoice : ',this.invoiceForm.value);
   }
 
 }
