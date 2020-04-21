127e599b2ee875b80d5e13dedae2b749035b0100
imaneslague <imane.sla.gue@gmail.com>
2020-04-21 23:48:39 +0000

    hospital

--- src/app/hospital/hopital/hopital.component.ts
+++ src/app/hospital/hopital/hopital.component.ts
@@ -20,6 +20,9 @@ export class HopitalComponent implements OnInit {
   hospitals: Hospital[];
   listhopitalCatValues: any;
   listHopital: any;
+  private rowSelection;
+  private IsRowSelected: boolean=false;
+  private IsMultple: boolean=false;
   error$: Observable<String>;
   dataSource: any;
   selection: SelectionModel<Hospital>;
@@ -39,6 +42,10 @@ export class HopitalComponent implements OnInit {
   iscolled: boolean = true
 
   constructor(private store: Store<any>, private _bottomSheet: MatBottomSheet, public dialog: MatDialog) {
+    this.delete=this.delete.bind(this);  
+
+    this.store.dispatch(new ActionsFiles.LoadHospital());
+    this.remplir()
 
     this.store.dispatch(new ActionsFile.LoadHospitalCat());
     this.store.subscribe(data => {
@@ -46,8 +53,8 @@ export class HopitalComponent implements OnInit {
       console.log(" this.listhopitalCatValues=> ", this.listhopitalCatValues)
 
     });
-    this.store.dispatch(new ActionsFiles.LoadHospital());
-    this.remplir()
+  
+
   }
   remplir() {
     this.store.subscribe(data => {
@@ -58,8 +65,6 @@ export class HopitalComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
       this.selection = new SelectionModel<Hospital>(true, []);
     })
-
-
   }
 
   applyFilter(filtervalue: string) {
@@ -86,46 +91,46 @@ export class HopitalComponent implements OnInit {
 
 
 
+  onrowselect(){
+    this.IsMultple= this.selection.selected.length>1;
+    this.IsRowSelected= this.selection.selected.length==1;
+}
+
+/** Whether the number of selected elements matches the total number of rows. */
+isAllSelected() {
+const numSelected = this.selection.selected.length;
+const numRows = this.dataSource.data.length; 
+return numSelected === numRows;
+}
+
+/** Selects all rows if they are not all selected; otherwise clear selection. */
+masterToggle() { 
+this.isAllSelected() ?
+    this.selection.clear() :
+    this.dataSource.data.forEach(row => this.selection.select(row)); 
+}
+
+/** The label for the checkbox on the passed row */
+checkboxLabel(row?: Hospital): string {
+
+if (!row) {
+  return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+}
+return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+}
 
 
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
 
+
+delete() {
+
+  // console.log("deleteselection",this.selection.selected);
+  // var RowId = localStorage.getItem("RowId")
+  if (confirm("Are You Sure You want to Delete the User?")) { 
+    var cat=<Hospital>this.selection.selected[0];
+    console.log("cat => ",cat);
+    this.store.dispatch(new ActionsFiles.DeleteHospital(cat.id)); 
+    // this.remplir() 
   }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row =>
-        this.selection.select(row),
-
-      );
-  }
-  onrowselect(row) {
-    console.log("roow", row.id)
-    localStorage.setItem("rows", row.id)
-  }
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: Hospital): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
-
-  }
-
-  isToggele() {
-    this.iscolled = !this.iscolled;
-  }
-  openDialog(data) {
-    this.dialog.open(DialogHospComponent, { data })
-
-  }
-  mnmmm() {
-    console.log("rowss", localStorage.getItem("rows"))
-    var rows = localStorage.getItem("rows")
-  }
+}
 }
