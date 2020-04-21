d2ccd5eeb55dc87273f496c80500c0dccd1934f6
rajaa elqasemy <elqasemyrajaa@gmail.com>
2020-04-21 00:14:57 +0000

    logo made

--- src/app/HospitalCategorie/Store/Action.ts
+++ src/app/HospitalCategorie/Store/Action.ts
@@ -2,13 +2,12 @@
 import { Store, Action } from '@ngrx/store'
 import { HospitalCat } from '../hospitalCat.model';
 import { Update } from '@ngrx/entity';
- 
 export enum HospitalCatActionType {
     LOAD = "[HospitalCat] load HospitalCat",
     LOAD_SUCCESS = "[HospitalCat] LOAD HospitalCat Success",
     LOAD_FAIL = "[HospitalCat] Load HospitalCat Fail ",
     AddAll = "AddAll",
-     
+
     CREATE = "[HospitalCat] Create HospitalCat",
     CREATE_SUCCESS = "[HospitalCat] Create HospitalCat Success",
     CREATE_FAIL = "[HospitalCat] Create HospitalCat Fail ",
@@ -17,98 +16,98 @@ export enum HospitalCatActionType {
     UPDATE_SUCCESS = "[HospitalCat] Update HospitalCat Success",
     UPDATE_FAIL = "[HospitalCat] Update HospitalCat Fail ",
 
-    DELETE= "[HospitalCat] Delete HospitalCat",
+    DELETE = "[HospitalCat] Delete HospitalCat",
     DELETE_SUCCESS = "[HospitalCat] Delete HospitalCat Success",
     DELETE_FAIL = "[HospitalCat] Delete HospitalCat Fail ",
 }
 
-export class LoadHospitalCat implements Action{
+export class LoadHospitalCat implements Action {
     readonly type = HospitalCatActionType.LOAD
 }
-export class LoadHospitalCatSuccess implements Action{
+export class LoadHospitalCatSuccess implements Action {
     readonly type = HospitalCatActionType.LOAD_SUCCESS;
 
- constructor(public payload: HospitalCat[]){ }
+    constructor(public payload: HospitalCat[]) { }
 
 }
-export class LoadHospitalCatFail implements Action{
+export class LoadHospitalCatFail implements Action {
     readonly type = HospitalCatActionType.LOAD_FAIL
-    constructor(public payload: string){}
+    constructor(public payload: string) { }
 }
 
 // Add HospitalCategory
 
-export class CreateHospitalCat implements Action{
+export class CreateHospitalCat implements Action {
     readonly type = HospitalCatActionType.CREATE;
- constructor(public payload: HospitalCat){}
+    constructor(public payload: HospitalCat) { }
 
 }
-export class CreateHospitalCatSuccess implements Action{
+export class CreateHospitalCatSuccess implements Action {
     readonly type = HospitalCatActionType.CREATE_SUCCESS;
-    constructor(public payload: HospitalCat){}
+    constructor(public payload: HospitalCat) { }
 }
-export class CreateHospitalCatFail implements Action{
+export class CreateHospitalCatFail implements Action {
     readonly type = HospitalCatActionType.CREATE_FAIL
-    constructor(public payload: string){}
+    constructor(public payload: string) { }
 }
 
 //Update hospitalCategory
-export class UpdateHospitalCat implements Action{
- readonly type = HospitalCatActionType.UPDATE;
- constructor(public payload: HospitalCat){     
-    console.log("Action Update",payload);
- }
-}
-
-export class UpdateHospitalCatSuccess implements Action{
-    readonly type = HospitalCatActionType.UPDATE_SUCCESS;
-    constructor(public payload: HospitalCat){        
-       console.log("Action update success=>",payload);
+export class UpdateHospitalCat implements Action {
+    readonly type = HospitalCatActionType.UPDATE;
+    constructor(public payload: HospitalCat) {
+        console.log("Action Update", payload);
     }
 }
 
-export class UpdateHospitalCatFail implements Action{
+export class UpdateHospitalCatSuccess implements Action {
+    readonly type = HospitalCatActionType.UPDATE_SUCCESS;
+    constructor(public payload: HospitalCat) {
+        console.log("Action update success=>", payload);
+    }
+}
+
+export class UpdateHospitalCatFail implements Action {
     readonly type = HospitalCatActionType.UPDATE_FAIL
-    constructor(public payload: string){
-       console.log("Action update fail",payload);
+    constructor(public payload: string) {
+        console.log("Action update fail", payload);
     }
 }
 
 // delete HospitalCategory
 
-export class DeleteHospitalCat implements Action{
+export class DeleteHospitalCat implements Action {
     readonly type = HospitalCatActionType.DELETE;
-    constructor(public payload: string){}
+    constructor(public payload: string) { }
 }
-export class DeleteHospitalCatSuccess implements Action{
+export class DeleteHospitalCatSuccess implements Action {
     readonly type = HospitalCatActionType.DELETE_SUCCESS;
 
- constructor(public payload: string){
-    console.log("Action delete success",payload);
- }
+    constructor(public payload: string) {
+        console.log("Action delete success", payload);
+    }
 
 }
-export class DeleteHospitalCatFail implements Action{
+export class DeleteHospitalCatFail implements Action {
     readonly type = HospitalCatActionType.DELETE_FAIL
-    constructor(public payload: string){
-        console.log("Action delete fail",payload);
+    constructor(public payload: string) {
+        console.log("Action delete fail", payload);
     }
 }
 
-export type HospitalCatAction=
-// LoadHospitalCat
- LoadHospitalCat |
- LoadHospitalCatSuccess | 
- LoadHospitalCatFail
-// CreateHospitalCat
- |CreateHospitalCat
- | CreateHospitalCatSuccess
- |CreateHospitalCatFail
-//  UpdateHospitalCat
- |UpdateHospitalCat
- |UpdateHospitalCatSuccess
- |UpdateHospitalCatFail
- // DeleteHospitalCat
- |DeleteHospitalCat
- |DeleteHospitalCatSuccess
- |DeleteHospitalCatFail;
+export type HospitalCatAction =
+    // LoadHospitalCat
+    LoadHospitalCat |
+    LoadHospitalCatSuccess |
+    LoadHospitalCatFail
+    // CreateHospitalCat
+    | CreateHospitalCat
+    | CreateHospitalCatSuccess
+    | CreateHospitalCatFail
+    //  UpdateHospitalCat
+    | UpdateHospitalCat
+    | UpdateHospitalCatSuccess
+    | UpdateHospitalCatFail
+    // DeleteHospitalCat
+    | DeleteHospitalCat
+    | DeleteHospitalCatSuccess
+    | DeleteHospitalCatFail;
--- src/app/hospital/hopital/hopital.component.ts
+++ src/app/hospital/hopital/hopital.component.ts
@@ -18,78 +18,78 @@ import { Hospital } from '../hospital.model';
 })
 export class HopitalComponent implements OnInit {
   hospitals: Hospital[];
-  listhopitalCatValues: any; 
+  listhopitalCatValues: any;
   listHopital: any;
   error$: Observable<String>;
   dataSource: any;
   selection: SelectionModel<Hospital>;
-  iscolled : boolean = true
+  iscolled: boolean = true
 
-  constructor(private store : Store<any>, private _bottomSheet: MatBottomSheet,public dialog: MatDialog) {
-    this.store.dispatch( new ActionsFile.LoadHospitalCat());
-    this.store.subscribe(data =>{  
-      this.listhopitalCatValues = Object.values(data.HospitalCat.entities)  
-      console.log(" this.listhopitalCatValues=> ",this.listhopitalCatValues) 
-    
+  constructor(private store: Store<any>, private _bottomSheet: MatBottomSheet, public dialog: MatDialog) {
+    this.store.dispatch(new ActionsFile.LoadHospitalCat());
+    this.store.subscribe(data => {
+      this.listhopitalCatValues = data
+      console.log(" this.listhopitalCatValues=> ", this.listhopitalCatValues)
+
     });
 
-    this.store.dispatch( new ActionsFiles.LoadHospital());
-    this.store.subscribe(data =>{  
-      this.listHopital = Object.values(data.Hospital.entities)  
-      console.log(" this.listhopital=> ",this.listHopital) ,
-      this.dataSource = new MatTableDataSource<Hospital>(this.listHopital);
+    this.store.dispatch(new ActionsFiles.LoadHospital());
+    this.store.subscribe(data => {
+      this.listHopital = Object.values(data.Hospital)
+      console.log(" this.listhopital=> ", this.listHopital),
+        this.dataSource = new MatTableDataSource<Hospital>(this.listHopital);
       this.dataSource.sort = this.sort;
       this.dataSource.paginator = this.paginator;
       this.selection = new SelectionModel<Hospital>(true, []);
     })
-  
+
   }
 
-  applyFilter(filtervalue : string){
+  applyFilter(filtervalue: string) {
     this.dataSource.filter = filtervalue.trim().toLowerCase();
   }
-  
-    
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  displayedColumns: string[] = [ 'select','countryHealthId', 'name','remark','history'];
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  displayedColumns: string[] = ['select', 'countryHealthId', 'name', 'remark', 'history'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   ngOnInit() {
-   
+
   }
-  get hospitalcat(){
-     return this.listhopitalCatValues;
+  get hospitalcat() {
+    return this.listhopitalCatValues;
   }
 
   openBottomSheet(): void {
     this._bottomSheet.open(HospitalEditComponent);
     console.log('show bottom sheet ...')
   }
- 
 
 
-  
+
+
 
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
-    
+
   }
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
     this.isAllSelected() ?
-        this.selection.clear() :
-        this.dataSource.data.forEach(row => 
-          this.selection.select(row),
-       
-        );
+      this.selection.clear() :
+      this.dataSource.data.forEach(row =>
+        this.selection.select(row),
+
+      );
   }
-  onrowselect(row){
-    console.log("roow",row.id)
-    localStorage.setItem("rows",row.id)
+  onrowselect(row) {
+    console.log("roow", row.id)
+    localStorage.setItem("rows", row.id)
   }
   /** The label for the checkbox on the passed row */
   checkboxLabel(row?: Hospital): string {
@@ -97,18 +97,18 @@ export class HopitalComponent implements OnInit {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
-    
+
   }
 
-  isToggele (){
-    this.iscolled= !this.iscolled;
+  isToggele() {
+    this.iscolled = !this.iscolled;
   }
   openDialog(data) {
     this.dialog.open(DialogHospComponent, { data })
-    
+
   }
-  mnmmm(){
-    console.log("rowss",localStorage.getItem("rows"))
+  mnmmm() {
+    console.log("rowss", localStorage.getItem("rows"))
     var rows = localStorage.getItem("rows")
   }
 }
