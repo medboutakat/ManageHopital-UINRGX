c79aa9e2ade59e9151c5922fd50b8c0b9bc84106
AbdelwahedK <57575879+AbdelwahedK@users.noreply.github.com>
2020-04-22 04:05:43 +0000

    add payment component to invoice list

--- src/app/payment/payment/payment.component.ts
+++ src/app/payment/payment/payment.component.ts
@@ -1,6 +1,8 @@
-import { Component, AfterViewChecked } from '@angular/core';
+import { Component, AfterViewChecked, Input } from '@angular/core';
 
 declare let paypal: any;
+
+
 
 @Component({
   selector: 'payment',
@@ -11,8 +13,8 @@ export class PaymentComponent implements AfterViewChecked {
 
   addScript: boolean = false;
   paypalLoad: boolean = true;
-  
-  finalAmount: number = 1;
+  @Input() finalAmount = 1;
+  //finalAmount: number = 1;
 
   paypalConfig = {
     env: 'sandbox',
@@ -33,12 +35,14 @@ export class PaymentComponent implements AfterViewChecked {
     onAuthorize: (data, actions) => {
       return actions.payment.execute().then((payment) => {
         //Do something when payment is successful.
+        //salert('Payment is ddddddoooonnnneeee')
       })
     }
   };
 
   ngAfterViewChecked(): void {
-    if (!this.addScript) {
+   
+    if (!this.addScript) { console.log('this.finalAmount : ',this.finalAmount)
       this.addPaypalScript().then(() => {
         paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
         this.paypalLoad = false;
@@ -56,9 +60,13 @@ export class PaymentComponent implements AfterViewChecked {
     })
   }
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    console.log('ngOnInit this.finalAmount : ',this.finalAmount)
+
   }
 
 }
