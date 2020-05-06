b2739e91bf6368875357787f43df064cf0258827
medboutakat <boutakatmohamed@gmail.com>
2020-05-06 18:00:51 +0000

    fix intercepteur

--- src/app/Auth/auth.interceptor.ts
+++ src/app/Auth/auth.interceptor.ts
@@ -1,28 +1,17 @@
 import { Injectable } from '@angular/core';
 import {  HttpEvent,  HttpHandler,  HttpInterceptor,  HttpRequest, HttpErrorResponse} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-// import { catch } 'rxjs/add/operator/catch';
-
-import 'rxjs/add/operator/catch';
+import { Router } from '@angular/router'; 
+ 
 
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor {
+export class AuthInterceptor implements HttpInterceptor {
   constructor(private router: Router) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { 
 
   
-    return next.handle(request)
-      // .catch((response: any) => {
-      //   if (response instanceof HttpErrorResponse && response.status === 401) {
-      //     localStorage.removeItem('token');
-      //     this.router.navigateByUrl('/log-in');
-      //   }
-      //   return Observable.throw(response);
-      // });
-;
+    return next.handle(request);
+     
   }
 }
