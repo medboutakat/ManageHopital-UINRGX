351005c562cdaf9311b3a97b0a180f36dae7c6cb
medboutakat <boutakatmohamed@gmail.com>
2020-05-10 22:51:34 +0000

    update home page

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
- interface link{
+ class link{
   txt:string;
-  url:string
+  url:string;
+
+  get path(){
+    return '/'+this.url;
+  }
 }
 
 @Component({
@@ -13,32 +17,51 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 })
 export class SidebarComponent implements OnInit {
   user = localStorage.getItem("user");
-  links: link[] = [
-    {txt:'Dashboard',url:'/'},
-    {txt:'Appointements',url:'/appointement'},
-    {txt:'Articles',url:'/articles'},
-    {txt:'Hospital',url:'/hospitals'},
-    {txt:'Hospital category',url:'/hospitalcat'},
-    {txt:'DoctorCategory',url:'/doctorCategory'},
-    {txt:'ProductCategory',url:'/productCategory'},
-    {txt:'Doctors',url:'/doctors'},
-    {txt:'Invoices',url:'/invoices'},
-    {txt:'Invoice',url:'/invoice'},
-    {txt:'Operations',url:'/operation'},
-    {txt:'Product',url:'/product'},
-    {txt:'material',url:'/material'},
-    {txt:'material edit',url:'/materialedit'},
-    {txt:'Payment',url:'/payment'},
-  ]
+
+
+  links: link[];
+
   constructor(
 
-  ) { }
+  ) { 
+
+
+    let menulinks = localStorage.getItem("links");
+    if(menulinks==null){
+     var defaultMenu = [
+        {txt:'home',url:'home'},
+        {txt:'Dashboard',url:''},
+        {txt:'Appointements',url:'appointement'},
+        {txt:'Articles',url:'articles'},
+        {txt:'Hospital',url:'hospitals'},
+        {txt:'Hospital category',url:'hospitalcat'},
+        {txt:'DoctorCategory',url:'doctorCategory'},
+        {txt:'ProductCategory',url:'productCategory'},
+        {txt:'Doctors',url:'doctors'},
+        {txt:'Invoices',url:'invoices'},
+        {txt:'Invoice',url:'invoice'},
+        {txt:'Operations',url:'operation'},
+        {txt:'Product',url:'product'},
+        {txt:'material',url:'material'},
+        {txt:'material edit',url:'materialedit'},
+        {txt:'Payment',url:'payment'},
+      ]
+
+      localStorage.setItem("links",JSON.stringify( defaultMenu));  
+    }
+
+  }
 
   ngOnInit() {
+   
+    let result=localStorage.getItem("links") 
+    this.links= JSON.parse(result); 
+
   }
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.links, event.previousIndex, event.currentIndex);
+    localStorage.setItem("links",JSON.stringify( this.links)); 
   }
 
 }
